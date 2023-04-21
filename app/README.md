236707e5d2a305eb34bed6c855579e6e5b2a060c
Thomas Verschoren <thomas@verschoren.com>
2023-04-21 21:41:06 +0200

    Initial commit

---
+++ app/README.md
@@ -0,0 +1,4 @@
+# Zendesk Messaging Profile Fixer
+
+This application is a proof of concept for an app that merges or fixes orphaned Messaging Profiles.
+See https://internalnote.com/deepdive-into-messaging-profiles/
---
+++ app/assets/main.js
@@ -0,0 +1,101 @@
+var client = ZAFClient.init();
+
+//Add Smooch App ID and API Token
+const smooch_app_id = '';
+const smooch_app_token = '';
+
+client.invoke('resize', { width: '100%', height: '100vh' });
+
+$(document).ready(async function() {
+    var requester = await client.get('ticket.requester').then(function(data) {
+    console.log(data);
+     return data['ticket.requester'];
+    });
+    console.log(requester);
+    $('#zendesk_user').html(JSON.stringify(requester,null, 2));
+    var requester_id = requester.id;
+    //get api/v2/users/identities
+    var identities = await client.request({
+        url: '/api/v2/users/' + requester_id + '/identities.json',
+        type: 'GET',
+        dataType: 'json'
+    }).then(function(data) {
+     return data['identities'];
+    });
+
+    //search for idenities with type messaging
+    var messagingIdentities = identities.filter(function(identity) {
+    return identity.type === 'messaging';
+    });
+
+    if (messagingIdentities[0]){
+        $('#zendesk_identity').html(JSON.stringify(messagingIdentities[0], null, 2));
+        var sunco_id = messagingIdentities[0].value;
+
+        var sunco_user = await client.request({
+            url: `https://api.smooch.io/v2/apps/${smooch_app_id}/users/${sunco_id}`,
+            type: 'GET',
+            dataType: 'json',
+            headers: {
+                'authorization': `Basic ${smooch_app_token}`
+            }
+        }).then(function(data) {
+            console.log(data);
+            return data;
+        });
+
+        console.log(sunco_user.user.profile);
+        $('#sunco_user').html(JSON.stringify(sunco_user.user, null, 2));
+    
+        // search for user with sunco_user.user.profile.email
+        var zendesk_user = await client.request({
+            url: '/api/v2/users/search.json?query=' + sunco_user.user.profile.email,
+            type: 'GET',
+            dataType: 'json'
+        }).then(function(data) {
+            console.log(data);
+            return data;
+        });
+
+        if (zendesk_user.users == 0){
+            $('#existing_user').html('No user found');
+            $('#create_user').attr('data-email', '');
+            $('#create_user').html('Create User');
+        }
+        else{
+            $('#existing_user').html(JSON.stringify(zendesk_user.users[0], null, 2));
+            $('#create_user').attr('data-target', zendesk_user.users[0].id);
+            $('#create_user').html('Merge User');
+        }
+    }
+
+    $('#create_user').click(async function(){
+        var zendesk_user_id = $(this).attr('data-target');
+        var zendesk_user_email = $(this).attr('data-email');
+        
+        console.log(zendesk_user_id, zendesk_user_email);
+        if (zendesk_user_id){
+            //Merge requester with zendesk_user_id
+            console.log('{"user": {"id": "' + zendesk_user_id + '"}}');
+            var merge_result = await client.request({
+                url: '/api/v2/users/' + requester_id + '/merge.json',
+                type: 'PUT',
+                dataType: 'json',
+                data: '{"user": {"id": "' + zendesk_user_id + '"}}'
+            }).then(function(data) {
+                console.log(data);
+                return data;
+            });
+        } else {
+            var update_user = await client.request({
+                url: '/api/v2/users/' + requester_id + '.json',
+                type: 'PUT',
+                dataType: 'json',
+                data: '{"user": {"email": "' + zendesk_user_email + '"}}'
+            }).then(function(data) {
+                console.log(data);
+                return data;
+            });
+        }
+    } );
+});
